6d6244613fb3dbd04d3114cd441795819741910c
Nicolas Ottavi <nicolas@agence-differente.fr>
2014-05-22 18:35:13 +0200

    ok

--- index.php
+++ index.php
@@ -7,16 +7,32 @@
  */
 
 require 'vendor/autoload.php';
-
+require_once 'vendor/os/php-excel/PHPExcel/IOFactory.php';
 use Symfony\Component\Finder\Finder;
-use
+
 
 $finder = new Finder();
+
+$fh = fopen(__DIR__."/email.txt","a+");
 
 $iterator = $finder->files()->name('*.xls*')->in('/Users/nottavi/Google Drive/Projets/Andros/NL21/');
 
 foreach ($iterator as $file) {
-
+        echo "loading ".$file->getRealPath()."\n";
+        $phpExcel = $objPHPExcel = PHPExcel_IOFactory::load( $file->getRealPath() );
+        $sheet = $objPHPExcel->getSheet(0);
+        foreach($sheet->getRowIterator() as $row) {
+            foreach ($row->getCellIterator() as $cell) {
+                //print_r($cell->getValue());
+                if( filter_var( $cell->getValue(), FILTER_VALIDATE_EMAIL) ):
+                    //echo $cell->getValue()."\n";
+                    fwrite($fh, $cell->getValue()."\n");
+                endif;
+            }
+        }
+        rename( $file->getRealPath(), $file->getRealPath().".done" );
 }
+
+fclose( $fh );
 
 ?>
